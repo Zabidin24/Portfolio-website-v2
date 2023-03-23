fe7c142c852b0b4c3378e363ae7947d055e73d1b
Zain Ul Abidin <118212795+Zabidin24@users.noreply.github.com>
2023-03-23 11:41:33 +0000

    Rendered dynamic data

--- app.py
+++ app.py
@@ -2,8 +2,42 @@ from flask import Flask, render_template
 
 
 app=Flask(__name__)
+
+Expertise=[
+{
+  'id': 1,
+  'title': 'Data Analysis',
+  'language': 'python',
+  'Packages': 'Pandas, NumPy'
+  },
+{
+  'id': 2,
+  'title': 'Machine Learning',
+  'language': 'python',
+  'Packages': 'Pandas, Scikit-learn'
+  },
+{
+  'id': 3,
+  'title': 'Deep Learning',
+  'language': 'python',
+  'Packages': 'Pandas, Pytorch'
+  },
+{
+  'id': 4,
+  'title': 'Dashboard Designing',
+  'language': 'DAX, Data modeling',
+  'Application': 'PowerBI'
+  }
+
+
+  
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         exp=Expertise, 
+                        page_name="portfolio")
 if __name__ =="__main__":
   app.run(host='0.0.0.0',debug=True)
