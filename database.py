1f852ed197706d6711f2a54c16759fcce68527a0
Zain Ul Abidin <118212795+Zabidin24@users.noreply.github.com>
2023-03-30 20:12:00 +0000

    Retrieved dictionary

--- database.py
+++ database.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine, text
+import mysql.connector
+from mysql.connector.cursor import MySQLCursor
+
 
 db_connection_string="mysql+pymysql://4cui1k5b88f03u1z2bwp:pscale_pw_AdiCfWfpwSeatb0oPIzeaTyVYsq04nlzzySxOSqp8KR@aws.connect.psdb.cloud/portfolio?charset=utf8mb4"
 
@@ -14,5 +17,11 @@ result_all=result.all()
 print("type(result all):",type(result_all))
 first_result=result_all[0]
 print("type(first_result:",type(first_result))
-  
-  
+
+
+
+def Convert(lst):
+ res_dct = map(lambda i: (lst[i], lst[i+1]), range(len(lst)-1)[::10])
+ return dict(res_dct)
+print("type(first_result_dict:",type(Convert(result_all)))
+print(Convert(result_all[2]))
